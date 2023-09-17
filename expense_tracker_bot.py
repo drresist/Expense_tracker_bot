13c2f82e13f4386949d15b184dcebf3dd5c150a7
Maksim Fesenko <drresist@gmail.com>
2023-09-17 16:03:36 +0300

    feat: function to add new category and reload it in bot

--- expense_tracker_bot.py
+++ expense_tracker_bot.py
@@ -3,7 +3,7 @@ from telebot import types
 from loguru import logger
 import os
 from dotenv import load_dotenv
-from gsheet import add_payment
+from gsheet import add_payment, get_categories
 
 load_dotenv()
 
@@ -17,10 +17,20 @@ bot = telebot.TeleBot(token)
 user_data = {}
 
 # Dictionary of categories for income and expense
+
 categories = {
     'Income': ['Salary', 'Freelance', 'Investment'],
     'Expense': ['Food', 'Transportation', 'Rent'],
 }
+
+@bot.message_handler(commands=['reload'])
+def reload_categories(message):
+    categories['Income'] = get_categories('Income')
+    categories['Expense'] = get_categories('Expense')
+    logger.info("Reloaded categories")
+    logger.info("New categories:")
+    logger.info(categories)
+
 
 
 @bot.message_handler(commands=['start'])
@@ -69,6 +79,56 @@ def handle_amount(message):
     # TODO: add to sql record
 
     user_data.clear()
+
+# Create a dictionary to store user-defined categories
+custom_categories = {}
+
+@bot.message_handler(commands=['addcategory'])
+def add_category(message):
+    bot.send_message(message.chat.id, "Enter the name of the new category in next format. Example\n"
+                                      "Income:auto")
+    bot.register_next_step_handler(message, process_new_category)
+    bot.send_message(message.chat.id, "Enter the name of the new category:")
+
+def process_new_category(message):
+    category_name = message.text
+    user_id = message.from_user.id
+
+    # Store the custom category for the user
+    custom_categories[user_id] = custom_categories.get(user_id, [])
+    custom_categories[user_id].append(category_name)
+
+    bot.send_message(message.chat.id, f"Category '{category_name}' added successfully!")
+
+# Modify the 'handle_category' function to include custom categories
+@bot.message_handler(func=lambda message: message.text in categories.get(user_data.get('category_type', ''), []) or message.text in custom_categories.get(message.from_user.id, []))
+def handle_category(message):
+    user_data['category'] = message.text
+    bot.send_message(message.chat.id, f"You selected: {user_data['category']}\n\nEnter the amount:")
+
+# Modify the 'view_categories' function to show custom categories
+@bot.message_handler(commands=['viewcategories'])
+def view_categories(message):
+    user_id = message.from_user.id
+    user_categories = custom_categories.get(user_id, [])
+
+    if user_categories:
+        categories_str = "\n".join(user_categories)
+        bot.send_message(message.chat.id, f"Your custom categories:\n{categories_str}")
+    else:
+        bot.send_message(message.chat.id, "You haven't added any custom categories yet.")
+
+@bot.message_handler(commands=['help'])
+def display_help(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    item_start = types.KeyboardButton("/start")
+    item_add_category = types.KeyboardButton("/addcategory")
+    item_view_categories = types.KeyboardButton("/viewcategories")
+    item_view_summary = types.KeyboardButton("/viewsummary")
+    markup.add(item_start, item_add_category, item_view_categories, item_view_summary)
+
+    commands_text = "Here are some commands you can use:"
+    bot.send_message(message.chat.id, commands_text, reply_markup=markup)
 
 
 if __name__ == '__main__':
--- gsheet.py
+++ gsheet.py
@@ -10,16 +10,28 @@ service_file = os.getenv("GOOGLE_SHEETS_KEY_FILE")
 if service_file is None:
     raise RuntimeError("GOOGLE_SHEETS_KEY_FILE is not set")
 
-gc = gspread.service_account(filename=os.getenv(service_file))
+gc = gspread.service_account(filename="creds/service_file.json")
 
 gc.login()
 sh = gc.open_by_key(os.getenv('SHEET_ID'))
 
 # Open the first worksheet (sheet1) by name
-worksheet = sh.sheet1
 
 
 def add_payment(category_type, category, amount):
+    worksheet = sh.sheet1
     # Append payment data to the worksheet
     worksheet.append_row([str(datetime.now()), category_type, category, amount])
     loguru.logger.info(f"Added payment: {category} - {amount}")
+
+def get_categories(category_type)->list:
+    worksheet = sh.get_worksheet(1)
+    loguru.logger.info(f"Getting categories: {category_type}")
+    if category_type is None:
+        return []
+    if category_type == "Income":
+        return worksheet.col_values(1)
+    if category_type == "Expense":
+        return worksheet.col_values(2)
+
+
